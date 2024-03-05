c2d8faa9bb79cfde2a9802fbbdada5951fb7b540
foskingson <foskingson@naver.com>
2024-03-05 15:31:49 +0900

    no message

--- Algorithmn/test.py
+++ Algorithmn/test.py
@@ -1,21 +1,43 @@
-from typing import List
-
-
 class Solution:
-    def minPathSum(self, grid: List[List[int]]) -> int:
-        
-        for i in range(len(grid)):
-            for k in range(len(grid[0])):
-                if i==0 and k==0:
-                    continue
-                if i==0:
-                    grid[i][k]=grid[i][k]+grid[i][k-1]
-                elif k==0:
-                    grid[i][k]=grid[i][k]+grid[i-1][k]
-                else:
-                    grid[i][k]= min(grid[i][k]+grid[i][k-1],grid[i][k]+grid[i-1][k])
-        return grid[-1][-1]
-                
+    def isNumber(self, s: str) -> bool:
+        s = s.lower()
+        if s.count('e') > 1:
+            return False
+        if 'e' not in s: 
+            return self.isDecimal(s) or self.isInteger(s)
+        else:
+            idx = s.find('e')
+            d_or_i = s[0:idx]
+            integer = s[idx+1:len(s)]
+            return (self.isDecimal(d_or_i) or (self.isInteger(d_or_i))) and self.isInteger(integer)
+
+
+    def isDecimal(self, s:str) -> bool: 
+        dot = digit = sign = False
+        for i in range(len(s)): 
+            if s[i].isdigit():
+                digit = True
+            elif s[i] in "+-":
+                if i != 0: 
+                    return False
+            elif s[i] == ".":
+                if dot: 
+                    return False
+                dot = True
+            else:
+                return False
+        return dot and digit
+    def isInteger(self, s:str) -> bool: 
+        digit = False
+        for i in range(len(s)):
+            if s[i].isdigit():
+                digit = True
+            elif s[i] in "+-":
+                if i != 0: 
+                    return False
+            else:
+                return False
+        return digit
 
 sol=Solution()
-print(sol.minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
+print(sol.isNumber("3.14e2"))
